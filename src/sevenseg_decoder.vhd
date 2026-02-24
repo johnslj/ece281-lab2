ef98c17e0c7f2a2e798390cfe5b714f2c3d7cb5d
johnslj <c26logan.johns@afacademy.af.edu>
2026-02-24 15:11:06 -0700

    decoder behavioral added

--- src/sevenseg_decoder.vhd
+++ src/sevenseg_decoder.vhd
@@ -39,6 +39,22 @@ end sevenseg_decoder;
 architecture Behavioral of sevenseg_decoder is
 
 begin
-
+    with i_Hex select
+    o_seg_n <= "00000011" when "0000",
+           "10011111" when "0001",
+           "00100101" when "0010",
+           "00001101" when "0011",
+           "10011001" when "0100",
+           "01001001" when "0101",
+           "01000001" when "0110",
+           "00011111" when "0111",
+           "00000001" when "1000",
+           "00001001" when "1001",
+           "00010001" when "1010",
+           "11000001" when "1011",
+           "11100101" when "1100",
+           "10000101" when "1101",
+           "11100001" when "1110",
+           "01110001" when "1111";
 
 end Behavioral;
